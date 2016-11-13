6d01bf33bed41273dad872132caaf1dc261639ec
catalindumitrache76 <catalindumitrache76@yahoo.es>
2016-11-13 17:23:34 +0100

    Checked behaviour and added QR button

--- team/src/main/resources/static/js/app.js
+++ team/src/main/resources/static/js/app.js
@@ -14,10 +14,14 @@ $(document).ready(
                             + "'>"
                             + msg.uri
                             + "</a></div>");
+                        $("#resultQR").html(
+                            "<button class='btn btn-lg btn-primary' type='submit'>QR Code</button>"
+                            );
                     },
                     error : function() {
                         $("#result").html(
-                                "<div class='alert alert-danger lead'>ERROR</div>");
+                                "<div class='alert alert-danger lead'>ERROR</div>"
+                                );
                     }
                 });
             });
