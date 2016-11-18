52dae71348217cebc03cf0ed57f87acae0156f3a
dbarelop <dbarelop@gmail.com>
2016-11-18 02:00:13 +0100

    Added Thymeleaf template engine

--- team/src/main/java/urlshortener/team/web/MetricsController.java
+++ team/src/main/java/urlshortener/team/web/MetricsController.java
@@ -3,24 +3,24 @@ package urlshortener.team.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import urlshortener.common.domain.ShortURL;
 import urlshortener.common.repository.ClickRepository;
 import urlshortener.common.repository.ShortURLRepository;
 import urlshortener.common.web.UrlShortenerController;
 import urlshortener.team.domain.Metrics;
 
+import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@RestController
+@Controller
 public class MetricsController {
 
     private static final Logger logger = LoggerFactory.getLogger(MetricsController.class);
@@ -31,15 +31,17 @@ public class MetricsController {
     private ClickRepository clickRepository;
 
     @RequestMapping(value = "/metrics/{hash}", method = RequestMethod.GET)
-    public ResponseEntity<Metrics> getMetrics(@PathVariable String hash) {
+    public String getMetrics(@PathVariable String hash, Model model, HttpServletResponse response) {
         logger.info("Requested metrics for link with hash " + hash);
         ShortURL shortURL = shortURLRepository.findByKey(hash);
         if (shortURL == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return null;
         }
         Long clicks = clickRepository.clicksByHash(hash);
         URI uri = linkTo(methodOn(UrlShortenerController.class).redirectTo(hash, null)).toUri();
         Metrics metrics = new Metrics(uri, shortURL, clicks);
-        return new ResponseEntity<>(metrics, HttpStatus.OK);
+        model.addAttribute("metrics", metrics);
+        return "metrics";
     }
 }
