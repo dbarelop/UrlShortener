2447d01e88eabe1350211725b60bc620242f7dbf
JohnD23 <david9352@hotmail.com>
2016-12-20 19:07:18 +0100

    Add class ShortName

--- team/src/main/java/urlshortener/team/domain/ShortName.java
+++ team/src/main/java/urlshortener/team/domain/ShortName.java
@@ -1,12 +1,11 @@
 package urlshortener.team.domain;
 
-
-import urlshortener.common.domain.ShortURL;
-
 public class ShortName{
 	
-	public ShortName(String id, ShortURL shortURL) {
+	public ShortName() {
        
+		
+		
     }
 	
 	
