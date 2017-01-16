47135a4ba723b0db0197b651744b8362574a0559
dbarelop <dbarelop@gmail.com>
2017-01-16 23:49:08 +0100

    Fixed suggestion service for similar words

--- team/src/main/java/urlshortener/team/service/SuggestionServiceImpl.java
+++ team/src/main/java/urlshortener/team/service/SuggestionServiceImpl.java
@@ -1,7 +1,9 @@
 package urlshortener.team.service;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -9,15 +11,20 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import org.springframework.util.ResourceUtils;
 import urlshortener.team.domain.ShortURL;
 import urlshortener.team.repository.ShortURLRepository;
 
 @Service
 public class SuggestionServiceImpl implements SuggestionService {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SuggestionServiceImpl.class);
+	private static final Logger logger = LoggerFactory.getLogger(SuggestionServiceImpl.class);
+
+	private File dictionary;
 	
 	@Autowired
 	protected ShortURLRepository shortURLRepository;
@@ -26,22 +33,20 @@ public class SuggestionServiceImpl implements SuggestionService {
 	
 	private List<String> words;	
 	
-	public SuggestionServiceImpl() {
+	public SuggestionServiceImpl() throws IOException {
+		dictionary = ResourceUtils.getFile("classpath:wordsEn.txt");
 		fillDictionary();
 	}
 
 	@Override
 	public List<String> getSuggestions(String userWord) {
 
-		//List<String> suggestions = words.stream().sorted(Comparator.comparingInt(s -> StringUtils.getLevenshteinDistance(s, userWord))).limit(10).collect(Collectors.toList());
-
 		List<String> similar = new ArrayList<>();
-		
+
 		if (!userWord.isEmpty()) {
 			for (String word : words) {
 				boolean coincidences = true;
 				for (int i = 0; i < userWord.length(); i++) {
-
 					if (userWord.length() <= word.length()) {
 						if (!userWord.toLowerCase().startsWith(String.valueOf(word.toLowerCase().charAt(i)), i)) {
 							coincidences = false;
@@ -51,12 +56,12 @@ public class SuggestionServiceImpl implements SuggestionService {
 				}
 				if (coincidences) {
 					if (userWord.length() < word.length()) {
-						if (uniqueId(word)) {
+						if (shortURLRepository.findByKey(word) == null) {
 							similar.add(word);
 							if (similar.size() > 3) {
 								return similar;
 							}
-						}					
+						}
 					}
 				}
 			}
@@ -71,39 +76,17 @@ public class SuggestionServiceImpl implements SuggestionService {
 		suggestions.addAll(similar);
 		suggestions.addAll(synonyms);
 
-		return suggestions;
+		return suggestions.stream().filter(s -> shortURLRepository.findByKey(s) == null).collect(Collectors.toList());
 	}
 	
-	private void fillDictionary(){
-
-		words = new LinkedList<>();
-		String file = "src/main/resources/wordsEn.txt";
-		try {
-			FileReader fr = new FileReader(file);
+	private void fillDictionary() throws IOException {
+		words = new ArrayList<>();
+		try (FileReader fr = new FileReader(dictionary)) {
 			BufferedReader br = new BufferedReader(fr);
-
 			String line;
 			while((line = br.readLine()) != null)
-				if(line.length() > 0)
-					words.add(line);
-
-			fr.close();
+				words.add(line);
 		}
-		catch(Exception e) {
-			LOG.error("Error leyendo fichero "+ file + ": " + e);
-		}
-
-	}
-	
-	private boolean uniqueId(String id) {
-		
-		ShortURL l = shortURLRepository.findByKey(id);
-		
-		if (l == null & !id.equals("")) {
-            return true;
-		}
-		
-		return false;
 	}
 
 }
