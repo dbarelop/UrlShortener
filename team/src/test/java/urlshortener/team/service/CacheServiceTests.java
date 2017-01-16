6eb54e86dbf929bd0a382afc1a3dcddde1ca9d3b
luisemz <luis_e.mosquera@uao.edu.co>
2017-01-16 15:10:04 +0100

    Functionality test F3

--- team/src/test/java/urlshortener/team/service/CacheServiceTests.java
+++ team/src/test/java/urlshortener/team/service/CacheServiceTests.java
@@ -2,6 +2,7 @@ package urlshortener.team.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -9,8 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+
+import urlshortener.team.domain.ShortURL;
+import urlshortener.team.repository.RuleRepository;
+import urlshortener.team.repository.ShortURLRepository;
+import urlshortener.team.repository.fixture.ShortURLFixture;
+import urlshortener.team.repository.fixture.VerificationRuleFixture;
 
 import java.net.URI;
 
@@ -25,37 +30,60 @@ public class CacheServiceTests {
 
 	@Value("${local.server.port}")
 	private int port = 0;
-    
+	@Autowired
+    private RuleRepository ruleRepository;
+	@Autowired
+    private ShortURLRepository shortURLRepository;
+
 	@Test
-	public void a_testStatusOK() throws Exception {
-		postBrandedLink("https://www.facebook.com/","facebook");
+	public void testStatusOK() throws Exception {
+		ShortURL shortURL = shortURLRepository.save(ShortURLFixture.url4());
 		Thread.sleep(11*1000);
-		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:" + this.port + "/facebook", String.class);
+		ResponseEntity<String> response = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port + "/" + shortURL.getHash(), String.class);
 		assertThat(response.getStatusCode(), is(HttpStatus.TEMPORARY_REDIRECT));
 		assertThat(response.getHeaders().getLocation(), is(new URI("https://www.facebook.com/")));
 	}
 	
 	@Test
 	public void testStatusNull() throws Exception {
-		postBrandedLink("https://www.facebok.com/","id1");
+		ShortURL shortURL = shortURLRepository.save(ShortURLFixture.url3());
 		Thread.sleep(11*1000);
-		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:" + this.port + "/id1", String.class);
+		ResponseEntity<String> response = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port + "/" + shortURL.getHash(), String.class);
 		assertThat(response.getStatusCode(), is(HttpStatus.TEMPORARY_REDIRECT));
-		assertThat(response.getHeaders().getLocation(), is(new URI("http://localhost:" + this.port + "/404/id1")));
+		assertThat(response.getHeaders().getLocation(), 
+				is(new URI("http://localhost:" + this.port + "/404/" + shortURL.getHash())));
 	}
 
 	@Test
-	public void z_testCacheServing() throws Exception {
+	public void testRulesInvalid() throws Exception {
+		ShortURL shortURL = shortURLRepository.save(ShortURLFixture.url2());
+		Thread.sleep(5*1000);
+		ruleRepository.save(VerificationRuleFixture.rule2());
 		Thread.sleep(11*1000);
-		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:" + this.port + "/cache/facebook", String.class);
-		assertTrue(response.hasBody());
-		assertThat(response.getStatusCode(), is(HttpStatus.OK));
-		assertThat(response.getHeaders().getLocation(), is(nullValue()));
+		ResponseEntity<String> response = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port + "/" + shortURL.getHash(), String.class);
+		assertThat(response.getStatusCode(), is(HttpStatus.TEMPORARY_REDIRECT));
+		assertThat(response.getHeaders().getLocation(), 
+				is(new URI("http://localhost:" + this.port + "/404/" + shortURL.getHash())));
 	}
-	
-	private ResponseEntity<String> postBrandedLink(String url, String id) {
-		MultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
-		parts.add("url", url); parts.add("shortName", id);
-		return new TestRestTemplate().postForEntity("http://localhost:" + this.port + "/brandedLink", parts, String.class);
+
+	@Test
+	public void testStatusBadAndCacheServing() throws Exception {
+		ShortURL shortURL = shortURLRepository.save(ShortURLFixture.url5());
+		Thread.sleep(11*1000);
+		shortURL.setLastStatus(null);
+		shortURLRepository.update(shortURL);
+		ResponseEntity<String> response1 = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port + "/" + shortURL.getHash(), String.class);
+		ResponseEntity<String> response2 = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port + "/cache/" + shortURL.getHash(), String.class);
+		
+		assertThat(response1.getStatusCode(), is(HttpStatus.TEMPORARY_REDIRECT));
+		assertThat(response1.getHeaders().getLocation(), 
+				is(new URI("http://localhost:" + this.port + "/404/" + shortURL.getHash())));
+		assertTrue(response2.hasBody());
+		assertThat(response2.getStatusCode(), is(HttpStatus.OK));
 	}
 }
